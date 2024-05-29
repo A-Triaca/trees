83b033fc396397ebcf4f4c303d41f8ae6f72dcd6
Alex Triaca <alex.triaca@pushsecurity.com>
2024-05-29 21:29:04 +0100

    Added binary tree remove method

--- binary_tree/binary_tree.go
+++ binary_tree/binary_tree.go
@@ -1,6 +1,8 @@
 package binary_tree
 
-import "golang.org/x/exp/constraints"
+import (
+	"golang.org/x/exp/constraints"
+)
 
 type Node[T constraints.Ordered] struct {
 	value T
@@ -41,5 +43,41 @@ func (n *Node[T]) Insert(nn *Node[T]) {
 
 // Remove the first occurrence of a value
 func (t *BinaryTree[T]) Remove(value T) {
+	if t.node == nil {
+		return
+	}
+	t.node = t.node.Remove(value)
+}
+
+func (n *Node[T]) Remove(value T) *Node[T] {
+	if n == nil {
+		return n
+	}
+
+	if value < n.value {
+		n.left = n.left.Remove(value)
+	} else if value > n.value {
+		n.right = n.right.Remove(value)
+	} else {
+		if n.left == nil {
+			return n.right
+		} else if n.right == nil {
+			return n.left
+		}
+
+		n.value = n.right.MinValue()
+
+		n.right = n.right.Remove(n.value)
+	}
+	return n
+}
+
+func (n *Node[T]) MinValue() T {
+	min := n.value
+	for n.left != nil {
+		min = n.left.value
+		n = n.left
+	}
+	return min
 
 }
--- binary_tree/binary_tree_test.go
+++ binary_tree/binary_tree_test.go
@@ -22,3 +22,52 @@ func TestInsert(t *testing.T) {
 		t.Errorf("Root right right value not 4. Got - %d", tree.node.value)
 	}
 }
+func TestRemove(t *testing.T) {
+	tree := BinaryTree[int]{}
+	tree.Insert(2)
+	tree.Insert(3)
+	tree.Insert(4)
+	tree.Insert(1)
+
+	tree.Remove(2)
+
+	if tree.node.value != 3 {
+		t.Errorf("Root value not 1. Got - %d", tree.node.value)
+	}
+	if tree.node.left.value != 1 {
+		t.Errorf("Root left value not 3. Got - %d", tree.node.value)
+	}
+	if tree.node.right.value != 4 {
+		t.Errorf("Root right value not 3. Got - %d", tree.node.value)
+	}
+	if tree.node.right.right != nil {
+		t.Errorf("Root right right not nil. Got - %d", tree.node.value)
+	}
+
+	tree.Remove(1)
+
+	if tree.node.value != 3 {
+		t.Errorf("Root value not 1. Got - %d", tree.node.value)
+	}
+	if tree.node.left != nil {
+		t.Errorf("Root left not nil. Got - %d", tree.node.value)
+	}
+	if tree.node.right.value != 4 {
+		t.Errorf("Root right value not 3. Got - %d", tree.node.value)
+	}
+
+	tree.Remove(4)
+
+	if tree.node.value != 3 {
+		t.Errorf("Root value not 1. Got - %d", tree.node.value)
+	}
+	if tree.node.right != nil {
+		t.Errorf("Root right not nil. Got - %d", tree.node.value)
+	}
+
+	tree.Remove(3)
+
+	if tree.node != nil {
+		t.Errorf("Root  not nil. Got - %d", tree.node.value)
+	}
+}
